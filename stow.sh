b32f63afcce95704d157b2a54a87d5181ccd1644
Jiaheng Tang <tjiaheng@vmware.com>
2020-04-14 22:29:32 +0800

    remove alacritty

--- stow.sh
+++ stow.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-dotfiles=(alacritty bash brew gdb git ranger skhd tmux vim yabai ycm zsh)
+dotfiles=(bash brew gdb git ranger skhd tmux vim yabai ycm zsh)
 
 for dotfile in "${dotfiles[@]}"; do
     stow -v -R -t ~ $dotfile
