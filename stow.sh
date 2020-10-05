7af8aaf30e940ace23d46dcf11e38ade5bab0a0f
Jiaheng Tang <tjiaheng@vmware.com>
2020-10-05 09:56:41 +0800

    update yabai

--- stow.sh
+++ stow.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
-dotfiles=(bash brew gdb git karabiner ranger skhd tmux vim yabai ycm zsh)
+dotfiles=(bash \
+          brew \
+          gdb \
+          git \
+          karabiner \
+          ranger \
+          skhd \
+          spacebar \
+          tmux \
+          vim \
+          yabai \
+          ycm \
+          zsh)
 
 for dotfile in "${dotfiles[@]}"; do
     stow -v -R -t ~ $dotfile
